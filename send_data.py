7f27cd90f96df49102bde03434e015b1fdc51aca
Mark Grobaker <mark427@gmail.com>
2020-09-28 13:41:41 +0000

    initial commit

---
+++ send_data.py
@@ -0,0 +1,14 @@
+import pulsar
+import time
+
+broker1_url = 'pulsar://ec2-18-223-193-14.us-east-2.compute.amazonaws.com:6650'
+
+client = pulsar.Client(broker1_url)
+producer = client.create_producer(topic='sensors')
+
+for i in range (10):
+	producer.send(('Hello-%d' %i).encode('utf-8'))
+	#time.sleep(100)
+
+client.close()
+
