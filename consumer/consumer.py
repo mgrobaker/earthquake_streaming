283226c56c569250f4ef1ff818ce58f94f776e84
Ubuntu <ubuntu@ip-10-0-0-10.us-east-2.compute.internal>
2020-10-07 13:51:35 +0000

    split code into functions

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,65 +3,85 @@ import json
 import psycopg2
 from psycopg2 import extras as psy_ext
 
-#initialize consumer
-broker1_url = 'pulsar://ec2-18-223-193-14.us-east-2.compute.amazonaws.com:6650'
-#client = pulsar.Client(broker1_url)
-#consumer = client.subscribe('sensors', 'sensor-subscription')
-
-#write to postgres
-x = [1, 2, 3]
-y = [4.2, 5.3, 6.4]
-z = [8, 9, 10]
-num_rows = len(x)
-insert_str = ['']
-
-###generate insertion string (make a function)
-#create the string
-#join it to the list
-for i in range(0, num_rows):
-    if i < num_rows - 1:
-        new_row = "({}, {}, {}), ".format(x[i], y[i], z[i])
-    else:
-        new_row = "({}, {}, {})".format(x[i], y[i], z[i])        
-
-    insert_str.append(new_row)
-
-final_str = ''.join(insert_str)    
-print("insert_str: " + final_str)
-    
-###
 conn = psycopg2.connect(
     host="localhost"
     , database="postgres"
     , user="pulsarcon"
     , password="a")
 
-cur = conn.cursor()
-#for i in range(1,num_rows):
+broker1_url = 'pulsar://ec2-18-223-193-14.us-east-2.compute.amazonaws.com:6650'
+    
+#make an insertion list like ((1,2,3), (4,5,6), (7,8,9))
+def make_insertion_list(x, y, z):
+    num_rows = max(len(x), len(y), len(z))
+    insertion_list = []
+    
+    for i in range(0, num_rows):
+        try:
+            xval = x[i]
+        except IndexError:
+            xval = 0
 
-psy_ext.execute_values(cur, "INSERT INTO samp_accel_values (x, y, z) VALUES %s",
-               [(x[2], 2, 3), (4, 5, 6), (7, 8, 9)])
+        try:
+            yval = y[i]
+        except IndexError:
+            yval = 0
 
-conn.commit()
-cur.close()
-conn.close()
+        try:
+            zval = z[i]
+        except IndexError:
+            zval = 0
+        
+        #add to list of rows
+        new_row = [xval, yval, zval]
+        insertion_list.append(new_row)
+        
+    return insertion_list
+            
+def insert_list(insertion_list):
+        print("insertion_list:")
+        print(insertion_list)
 
-"""
-while True:
-    msg = consumer.receive()
-    try:
-        msg_data = msg.data()
-        print("Received message '{}' id='{}'".format(msg_data, msg.message_id()))
-        # Acknowledge successful processing of the message
-        consumer.acknowledge(msg)
-        print('attempting parsing...')
-        data_str = msg_data.decode('utf8')
-        data_dict = json.loads(data_str)
-        #data_dict_str = json.dumps(data_dict)
-        print(data_dict['x'])
-    except:
-        # Message failed to be processed
-        consumer.negative_acknowledge(msg)
+        cur = conn.cursor()
+        #insert all the values
+        psy_ext.execute_values(cur, "INSERT INTO samp_accel_values VALUES %s", insertion_list)
+        conn.commit()
+        cur.close()
 
-client.close()
-"""
+def listen():
+    
+    while True:
+        msg = consumer.receive()
+        try:
+            msg_data = msg.data()
+            print("Received message '{}' id='{}'".format(msg_data, msg.message_id()))
+            # Acknowledge successful processing of the message
+            consumer.acknowledge(msg)
+            print('attempting parsing...')
+            data_str = msg_data.decode('utf8')
+            data_dict = json.loads(data_str)
+            #data_dict_str = json.dumps(data_dict)
+            print(data_dict['x'])
+        except:
+            # Message failed to be processed
+            consumer.negative_acknowledge(msg)
+
+        client.close()
+
+def main():
+    x = [1, 2, 3]
+    y = [4.2, 5.3, 6.4]
+    z = [8, 9]
+
+    insertion_list = make_insertion_list(x, y, z)
+    insert_list(insertion_list)
+
+    #initialize consumer
+    #client = pulsar.Client(broker1_url)
+    #consumer = client.subscribe('sensors', 'sensor-subscription')
+    
+    conn.close()
+
+if __name__ == "__main__":
+    main()
+        
