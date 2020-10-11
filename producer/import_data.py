7206d26b15dc476ea107fc385bdd8288ea7a71ff
Mark Grobaker <5614366+mgrobaker@users.noreply.github.com>
2020-10-11 17:59:27 +0000

    working on downloading files, to speed up transmission

--- producer/import_data.py
+++ producer/import_data.py
@@ -4,53 +4,64 @@ from pulsar.schema import *
 import time
 import json
 
+#initialize pulsar producer
 broker1_url = 'pulsar://ec2-18-223-193-14.us-east-2.compute.amazonaws.com:6650'
 client = pulsar.Client(broker1_url)
+producer = client.create_producer(topic='sensors')
 
+#initialize bucket variables
 s3 = boto3.resource('s3')
-#response = s3.Bucket(BUCKET_NAME).objects.filter(Prefix=PREFIX)
-
 BUCKET_NAME = 'grillo-openeew'
-PREFIX = 'records/country_code=mx/device_id=010/year=2020/month=09/day=17/hour=02/'
-OBJ_NAME = '01.jsonl'
-FULL_PATH = PREFIX + OBJ_NAME
+OBJ_NAME = '00.jsonl'
 
-#initialize Pulsar producer
-###producer = client.create_producer(topic='sensors', schema=JSONSchema())
-#producer = client.create_producer(topic='sensors')
+#make lists of months, days, and hours, to iterate through
+months_list = list(range(1,13))
+#days_list = range(1,31)
+days_list = list(range(10,15))
+hours_list = list(range(0,24))
 
+#convert to 0-padded strings, as required by openeew
+for i in range(0, len(months_list)):
+    months_list[i] = '{0:02}'.format(months_list[i])
+for i in range(0, len(days_list)):
+    days_list[i] = '{0:02}'.format(days_list[i])
+for i in range(0, len(hours_list)):
+    hours_list[i] = '{0:02}'.format(hours_list[i])
 
-months_list = range(1,13)
-days_list = range(1,31)
-hours_list = range(0,24)
+files_sent = 0
+#add timing logic
 
-#reformat
-for hour in hours_list: hour = '{0:02}'.format(hour)
+#also, i have to iterate through all the JSON files. not just 00
 
-#get one of the objects
+#iterate through the objects in the bucket
+for day in days_list:
+    #print('day: {}'.format(day))
+    for hour in hours_list:
+        print('HOUR: {}'.format(hour))
+        BUCKET_PATH = 'records/country_code=mx/device_id=010/year=2020/month=09/day={}/hour={}/{}'.format(day,hour,OBJ_NAME)
 
-#for day in days_list:
-for hour in hours_list:
-    hour_s = '{0:02}'.format(hour)
-    print('HOUR: {}'.format(hour_s))
-    PREFIX = 'records/country_code=mx/device_id=010/year=2020/month=09/day=17/hour={}/'.format(hour_s)
+        try:
+            s3obj = s3.Object(BUCKET_NAME, BUCKET_PATH).get()['Body'].read()
 
-    try:
-        s3obj = s3.Object(BUCKET_NAME, FULL_PATH).get()['Body'].read()
-        sensor_rdgs = s3obj.splitlines()
+            #download file
+            s3.Bucket(BUCKET_NAME).download_file(BUCKET_PATH, 'test_download.jsonl')
+            
+            ##sensor_rdgs = s3obj.splitlines()
+            print('sent: {}'.format(PATH))
+            files_sent += 1
 
-        #send messages
-        i = 0
-        for line in sensor_rdgs:
-            print(line)
-            #producer.send(line)
-        
-            i += 1
-            if i > 3: break
-    except:
-        #if no file is found at this location, skip
-        #this prevents producer from breaking due to invalid path
-        continue
+            #send messages
+            i = 0
+            #for line in sensor_rdgs:
+                #print(line)
+                ##producer.send(line)
 
-    
+                #i += 1
+                #if i > 3: break
+        except:
+            #if no file is found at this location, skip
+            #this prevents producer from breaking due to invalid path
+            continue
+
+print('NUM FILES SENT: {}'.format(files_sent))
 client.close()
