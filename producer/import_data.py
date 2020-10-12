ce3e4b6fc8b5b0d8efe5e05f497906a88f2b7c9e
Mark Grobaker <5614366+mgrobaker@users.noreply.github.com>
2020-10-12 13:46:38 +0000

    now downloads sensor metadata and loops through

--- producer/import_data.py
+++ producer/import_data.py
@@ -1,20 +1,21 @@
 import boto3, botocore
+import json
 
 #initialize bucket variables
 s3 = boto3.resource('s3')
 BUCKET_NAME = 'grillo-openeew'
 OBJ_NAME = '00.jsonl'
-#TODO: i have to iterate through all the JSON files. not just 00
 
 YEAR = '2020'
-device_id = '010'
+MONTH = '09'
+#so, we want to scale up by 70. 30 devices, and double the days (1 week)
 
-def set_loop_vars():
+def set_download_loop_vars():
     #make lists of months, days, and hours, to iterate through
     global months_list, days_list, hours_list
     months_list = list(range(1,13))
     #days_list = range(1,31)
-    days_list = list(range(1,4))
+    days_list = list(range(1,8))
     hours_list = list(range(0,24))
     
     #convert to 0-padded strings, as required by openeew
@@ -32,29 +33,60 @@ def download_data():
     for day in days_list:
         #print('day: {}'.format(day))
         for hour in hours_list:
-            BUCKET_PATH = 'records/country_code=mx/device_id={}/year={}/month={}/day={}/hour={}/{}'.format(device_id, YEAR, months_list[8], day, hour, OBJ_NAME)
-            #print('HOUR: {}'.format(hour))
-            #print('{}'.format(BUCKET_PATH))
+            for device_id in device_id_list:
+                BUCKET_PATH = 'records/country_code=mx/device_id={}/year={}/month={}/day={}/hour={}/{}'.format(device_id, YEAR, MONTH, day, hour, OBJ_NAME)
+                #print('HOUR: {}'.format(hour))
+                #print('{}'.format(BUCKET_PATH))
 
-            try:
-                #download file
-                download_file_name = '../input_data/device{}_yr{}_mon{}_day{}_hr{}_{}'.format(device_id, YEAR, months_list[8], day, hour, OBJ_NAME)
-                
-                s3.Bucket(BUCKET_NAME).download_file(BUCKET_PATH, download_file_name)
+                try:
+                    #download file
+                    download_file_name = '../input_data/device{}_yr{}_mon{}_day{}_hr{}_{}'.format(device_id, YEAR, MONTH, day, hour, OBJ_NAME)
 
-                print('downloaded: {}'.format(BUCKET_PATH))
-                files_downloaded += 1
+                    s3.Bucket(BUCKET_NAME).download_file(BUCKET_PATH, download_file_name)
 
-            except:
-                #if no file is found at this location, skip
-                #this way, the code doesn't fail due to a malformed path
-                continue
+                    print('downloaded: {}'.format(BUCKET_PATH))
+                    files_downloaded += 1
+
+                except:
+                    #if no file is found at this location, skip
+                    #this way, the code doesn't fail due to a malformed path
+                    continue
 
     print('NUM FILES DOWNLOADED: {}'.format(files_downloaded))
 
-def main():
-    set_loop_vars()
-    download_data()
+def download_device_list():
+    file_name = 'devices.jsonl'
+    BUCKET_PATH = 'devices/country_code=mx/{}'.format(file_name)
 
+    s3.Bucket(BUCKET_NAME).download_file(
+        BUCKET_PATH, '../input_sensor_list/{}'.format(file_name))
+
+def parse_device_list():
+    #make list for each col, go thru lines, and append to it
+    global device_id_list
+    device_id_list = []
+    latitude_list = []
+    longitude_list = []
+
+    #open file
+    file_name = 'devices.jsonl'        
+    file = open('../input_sensor_list/{}'.format(file_name),'r').read()
+    file_lines = file.splitlines()
+    
+    for line in file_lines:
+        line_cols = json.loads(line)
+        if line_cols['is_current_row']:
+            #print('device_id: {}'.format(line_cols['device_id']))
+            device_id_list.append(line_cols['device_id'])
+            latitude_list.append(line_cols['latitude'])
+            longitude_list.append(line_cols['longitude'])
+            
+def main():
+    download_device_list()
+    parse_device_list()
+
+    set_download_loop_vars()
+    download_data()
+    
 if __name__ == "__main__":
     main()
