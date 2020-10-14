4f07c8736ef7c3d9d9b5be4eff187f2baf250a04
Mark Grobaker <5614366+mgrobaker@users.noreply.github.com>
2020-10-14 20:19:31 +0000

    minor updates for testing

--- producer/test_producer.py
+++ producer/test_producer.py
@@ -1,8 +1,24 @@
 import pulsar
+import time
 
 broker1_url = 'pulsar://ec2-18-223-193-14.us-east-2.compute.amazonaws.com:6650'
-client = pulsar.Client(broker1_url)
-producer = client.create_producer(topic='sensors')
+#broker2_url = 'pulsar://ec2-3-19-119-137.us-east-2.compute.amazonaws.com:6650' 
 
-for i in range (10):
+client = pulsar.Client(broker1_url)
+producer = client.create_producer(topic='sensors', batching_enabled=True)
+
+def callback(res, msg):
+    return
+    
+t0 = time.time()
+num_msgs = 5000
+for i in range (num_msgs):
+#    producer.send_async(('Hello-%d' %i).encode('utf-8'), callback)
     producer.send(('Hello-%d' %i).encode('utf-8'))
+
+t1 = time.time()
+
+send_time = t1-t0
+
+print('NUM MSGS SENT: {}'.format(num_msgs))
+print('SEND TIME: {}'.format(send_time))
