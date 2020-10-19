588a74e14b757fc3ee80947816e47f8f75cea5b9
Mark Grobaker <ubuntu@ip-10-0-0-10.us-east-2.compute.internal>
2020-10-19 15:42:14 +0000

    removed, as this is not in use

--- pulsar_functions/src/main/java/hello/ExclamationFunction.java
+++
@@ -1,9 +0,0 @@
-import org.apache.pulsar.functions.api.Context;
-import org.apache.pulsar.functions.api.Function;
-
-public class ExclamationFunction implements Function<String, String> {
-    @Override
-    public String process(String input, Context context) {
-        return String.format("%s!", input);
-    }
-}
